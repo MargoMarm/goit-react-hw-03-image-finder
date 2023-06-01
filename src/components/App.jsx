11e55d2b5c18fe368ae8343edf6b8202a208c073
MargoMarm <margogerasimenko@gmail.com>
2023-06-01 17:02:49 +0200

    add searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Searchbar from './Searchbar/Searchbar';
+import { getPictures } from './services/getPictures';
+
+export class App extends Component {
+  state = {
+    serachQuery: '',
+  };
+
+	handleSerach = (serachQuery) => {
+		this.setState({serachQuery})
+	}
+	
+  render() {
+    return (
+      <div>
+			 <Searchbar onSubmit={ this.handleSerach} />
+      </div>
+    );
+  }
+}
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,49 @@
+import { Component } from 'react';
+import {
+  Form,
+  FormButton,
+  ButtonLabel,
+  FormInput,
+  Header,
+} from './Searchbar.styled';
+
+class Searchbar extends Component {
+  state = {
+    value: '',
+  };
+
+  handleChange = ({ target: { value } }) => {
+    this.setState({ value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.props.onSubmit(this.state.value);
+    this.setState({ value: '' });
+    console.log(e);
+  };
+
+  render() {
+    return (
+      <Header>
+        <Form type="submit" onSubmit={this.handleSubmit}>
+          <FormButton>
+            <ButtonLabel>Search</ButtonLabel>
+          </FormButton>
+
+          <FormInput
+            onChange={this.handleChange}
+            type="text"
+            autocomplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            value={this.state.value}
+          />
+        </Form>
+      </Header>
+    );
+  }
+}
+
+export default Searchbar;
